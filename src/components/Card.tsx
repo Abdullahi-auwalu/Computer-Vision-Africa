f85cb3c1d45fe28a7da9a24e837b816dc1d542c4
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2024-11-13 14:32:48 +0100

    fix a bug

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ text, image }) => {
   return (
-    <div className="w-[450px] md:w-[500px] h-[435px md:h-[535px] bg-white mt-8 shadow-lg shadow-gray-500/50 pb-4">
+    <div className="w-[450px] md:w-[500px] md:h-[535px] bg-white mt-8 shadow-lg shadow-gray-500/50 pb-4">
       <Image
         src={image}
         alt=""
@@ -15,7 +15,7 @@ const Card: React.FC<CardProps> = ({ text, image }) => {
         height={300}
         className="w-[500px] h-[400px]"
       />
-      <h3 className="pt-4 text-2xl px-[50px] font-semibold text-center">
+      <h3 className="pt-4 text-2xl p-[50px] font-semibold text-center">
         {text}
       </h3>
     </div>
